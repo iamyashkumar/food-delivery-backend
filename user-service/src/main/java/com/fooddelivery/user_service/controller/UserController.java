99b3b2c046d1d0e0b86bbccf93a0e05de2c23f7d
Yash Kumar <yashkumarkanha@gmail.com>
2026-04-07 23:33:33 +0530

    Day 3: UserController with JSON API - /api/users/all

---
+++ user-service/src/main/java/com/fooddelivery/user_service/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.fooddelivery.user_service.controller;
+
+import com.fooddelivery.user_service.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController{
+    @GetMapping("/all")
+    public List<User> getAllUser() {
+        List<User> users = new ArrayList<>();
+        users.add(new User (1L, "Yash Kumar", "yash@gamil.com", "pass123"));
+        users.add(new User(2L, "Rahul Singh", "rahul@gmail.com", "pass456"));
+        users.add(new User(3L, "Priya Sharma", "priya@gmail.com", "pass789"));
+        return users;
+    }
+}
