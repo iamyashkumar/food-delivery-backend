370731019eb8a169c45ba2b0e01ac1dd3b3cb2ee
Yash Kumar <yashkumarkanha@gmail.com>
2026-04-13 23:53:15 +0530

    Day 5: POST API with @RequestBody - addUser endpoint

--- user-service/src/main/java/com/fooddelivery/user_service/controller/UserController.java
+++ user-service/src/main/java/com/fooddelivery/user_service/controller/UserController.java
@@ -1,36 +1,37 @@
 package com.fooddelivery.user_service.controller;
 
 import com.fooddelivery.user_service.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController{
+    private List<User> userList = new ArrayList<>();
     @GetMapping("/all")
     public List<User> getAllUser() {
-        List<User> users = new ArrayList<>();
-        users.add(new User(1L, "Yash Kumar", "yash@gamil.com", "pass123"));
-        users.add(new User(2L, "Rahul Singh", "rahul@gmail.com", "pass456"));
-        users.add(new User(3L, "Priya Sharma", "priya@gmail.com", "pass789"));
-        return users;
+        userList.add(new User(1L, "Yash Kumar", "yash@gmail.com", "pass123"));
+        userList.add(new User(2L, "Rahul Singh", "rahul@gmail.com", "pass456"));
+        userList.add(new User(3L, "Priya Sharma", "priya@gmail.com", "pass789"));
+        return userList;
     }
      @GetMapping("/{id}")
      public User getUserById(@PathVariable Long id) {
-        List<User> users = new ArrayList<>();
-         users.add(new User(1L, "Yash Kumar", "yash@gmail.com", "pass123"));
-         users.add(new User(2L, "Rahul Singh", "rahul@gmail.com", "pass456"));
-         users.add(new User(3L, "Priya Sharma", "priya@gmail.com", "pass789"));
-         for (User user : users) {
-             if (user.getId().equals(id)){
+         for (User user : userList) {
+             if (user.getId().equals(id)) {
                  return user;
              }
          }
          return null;
      }
-}
+         @PostMapping
+         public String addUser(@RequestBody User user) {
+             userList.add(user);
+             return "User added successfully!";
+         }
+         }
+
