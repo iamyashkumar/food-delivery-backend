39ff9ace4a1d41e70417706c4751bd64c80e6e5b
Yash Kumar <yashkumarkanha@gmail.com>
2026-04-14 11:38:08 +0530

    Day 6: DELETE API with @DeleteMapping - deleteUser endpoint

--- user-service/src/main/java/com/fooddelivery/user_service/controller/UserController.java
+++ user-service/src/main/java/com/fooddelivery/user_service/controller/UserController.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController
 @RequestMapping("/api/users")
@@ -30,5 +31,10 @@ public class UserController{
              userList.add(user);
              return "User added successfully!";
          }
+         @DeleteMapping("/{id}")
+         public String deleteUser(@PathVariable Long id) {
+             userList.removeIf(user -> user.getId().equals(id));
+             return "User deleted successfully!";
          }
+     }
 
